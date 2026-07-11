5090c51f5ecfceec3522eeeb6ffc33df24fbccb7
M-Saad-K <mskscotland@gmail.com>
2026-07-11 21:59:31 +0100

    Boolean and arithmetric, now on 1.5

--- Part1.py
+++ Part1.py
@@ -11,7 +11,8 @@ This page covers task from 1.1 -> 1.6
 import sys
 
 def main():
-    practise1()
+    practise2()
+
 
 
 def practise0():
@@ -41,6 +42,26 @@ def practise1():
         prin = prin * (rate + 1)
         print(year, prin)
         year+=1
+
+def practise2():
+    # Bit Manipulation
+    x : int = 1
+    print(x << 1) # Left shift goes up
+    print(x >> 1) # Right shift goes down
+
+    """
+        3          =>  00011
+      & 5          =>  00101
+    ------           -------
+        1              00001
+    """
+    # bitwise and, if two areas have 1, then 1
+    print(12 & 20)
+    # | ^ -x (or, xor, neg)
+    y = 100001
+    print(f"y shift 5 times is to the right {y >> 5}")
+
+
 
 if __name__ == '__main__':
     main()
