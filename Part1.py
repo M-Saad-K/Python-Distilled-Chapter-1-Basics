6f25db1fc00ac2128ef69860760df92efcc8db66
M-Saad-K <mskscotland@gmail.com>
2026-07-11 21:40:40 +0100

    Completed to 1.4

--- Part1.py
+++ Part1.py
@@ -8,3 +8,39 @@ Description:
 This page covers task from 1.1 -> 1.6
 """
 
+import sys
+
+def main():
+    practise1()
+
+
+def practise0():
+    # Primitive
+    x = 42
+    b : int = 42
+    y = 4.2
+    z = 'forty-two'
+    a = True
+
+    i: int = 0
+
+    # While
+
+    while i <= 4:
+            print("This is i: ", i)
+            i=i+1
+
+
+def practise1():
+    prin: int = 1000
+    rate: float = 0.05
+    numyears: int = 5
+    year: int = 1
+
+    while year < numyears:
+        prin = prin * (rate + 1)
+        print(year, prin)
+        year+=1
+
+if __name__ == '__main__':
+    main()
