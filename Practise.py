2384568f46f4680a585fc96f9858b590a7f5cd8b
M-Saad-K <mskscotland@gmail.com>
2026-07-13 13:51:12 +0100

    Completed 1.7

--- Practise.py
+++ Practise.py
@@ -12,7 +12,7 @@ This page covers task from 1.1 -> 1.6
 import sys
 
 def main():
-    practise7()
+    practise8()
 
 
 
@@ -144,6 +144,42 @@ def practise7():
     x: int = 5
     forcast: str = "                           Weather is " + str(x) + "*c\nGlasgow, Scotland                                                                    "
     print(forcast.strip())
+
+def practise8():
+
+    data: str = " "
+
+    ### This is for each line
+
+    with open('input.txt') as file: #Open func returns a file object
+        # The with statement is used to not have to close it again
+        for line in file: # For each line in the file
+            print(line, end='') # Print each line, omitting the extra newline
+
+
+    ### This is for entirity
+    with open('input.txt') as file:
+        data = file.read()
+        print(data)
+
+    ### large file in chunks, for larger files, you can assign them
+    # The chunk will read for 4 blocks
+    with open('input.txt') as file:
+        while(chunk := file.read(4)): # The := assigns a value and returns a value for the while loop to use as well
+            print(chunk, end='')
+
+
+    # Outputting to a file
+    data = data.replace("Moon", "ISS")
+    with open('output.txt', 'wt') as output: # wt is writing
+        # print(data, file=output) # But printing is for additional words using while, like the example on pg13
+        output.write(data)
+    
+    name = input("What is your name: ")
+    print(name)
+
+    
+
 
 if __name__ == '__main__':
     main()
