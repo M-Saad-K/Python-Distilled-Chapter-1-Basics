9ed583116f62882b43f52bb10ae14ed9181ddac0
M-Saad-K <mskscotland@gmail.com>
2026-08-02 16:23:04 +0100

    Adding improvements

--- Practise.py
+++ Practise.py
@@ -394,7 +394,6 @@ class tree:
     
         if node.getRight() is not None:
             self.inOrder(node.getRight())
-    def inOrder(self, node):
 
     def getValue(self) -> int:
         if self._item == None:
