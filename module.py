8e5ee7a51fc0756abf52930e363b785218e01221
M-Saad-K <mskscotland@gmail.com>
2026-08-04 12:01:15 +0100

    Made read_portfolio

---
+++ module.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+
+    return
+
+def read_portfolio(filename):
+    portfolio : int = []
+
+    with open(filename) in file:
+        for line in file:
+            row = line.split(", ")
+            for word in row:
+                portfolio.append(word)
+
+    return portfolio
